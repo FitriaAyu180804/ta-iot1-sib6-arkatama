d914fb66c9052b9580a69f5a9c2898836df72454
Fitria <fitria180804@gmail.com>
2024-06-09 21:32:56 +0700

    add route put in api led

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::group(['as' => 'api.'], function () {
                 ->name('show');
             Route::post('/', [LedController::class, 'store'])
                 ->name('store');
-            Route::put('/', [LedController::class, 'update'])
+            Route::put('/{id}', [LedController::class, 'update'])
                 ->name('update');
             Route::delete('/{id}', [LedController::class, 'destroy'])
                 ->name('destroy');
